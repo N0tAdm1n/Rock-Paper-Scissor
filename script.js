1a17878616cedbff6cb9f6cc20733315ead987be
N0tAdm1n <n0tadm1n29@gmail.com>
2022-01-20 15:22:52 +0530

    Added userInput()

--- script.js
+++ script.js
@@ -7,15 +7,25 @@ function computerPlay() {
 }
 //A function to generate random number between 0 to 100(exclusive)
 let randomNumGen = () => {return Math.random() * 100};
-
+// Play a round of rock paper scissor
 function playRound() {
-    let playerSelection = prompt("Choose [Rock, Paper, Scissor] : ");
+    playerSelection = userInput();
     computerSelection = computerPlay().toUpperCase();
     if (playerSelection === "ROCK" && computerSelection === "SCISSOR") return `You win!, ${playerSelection} beats ${computerSelection}`;
     else if (playerSelection === "Scissor" && computerSelection === "PAPER") return `You win!, ${playerSelection} beats ${computerSelection}`;
-    else if (playerSelection === "Paper" && computerSelection === "ROCK") return `You win!, ${playerSelection} beats ${computerSelection}`;
+    else if (playerSelection === "PAPER" && computerSelection === "ROCK") return `You win!, ${playerSelection} beats ${computerSelection}`;
     else if (playerSelection === computerSelection) return "Nani!!! Its a draw";
     else return `You lose! ${computerSelection} beats ${playerSelection}`;
+}
+
+function userInput() {
+    let result = prompt("Choose [Rock, Paper, Scissor] : ").toUpperCase();
+    if(result === "ROCK" || result === "PAPER" || result === "SCISSOR") {
+        return result;
+    }
+    else {
+        userInput();
+    }
 }
 
 function game() {
